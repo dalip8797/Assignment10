a2fa7fc7023b66f0eca1f4f918fc73c0254eefe8
dalip8797 <dalip8797@gmail.com>
2018-06-20 09:54:30 +0530

    FileReadProgram

---
+++ FileReadProgram.java
@@ -0,0 +1,16 @@
+Package fileread;
+import java.io.*;
+public class static void main(String[] args) throw IOException
+}
+File d=new File)("C:\\Users\User\\Desktop\\class.txt");
+FileInputStream in;
+{
+in=new FileInputStream(d);
+int c;
+while((c=in.read())!=-1)
+{
+System.out.println(" "+(char)c)
+}
+}
+}
+}
